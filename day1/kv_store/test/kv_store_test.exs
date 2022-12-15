5f232f6c54bc360317f91d1e0094a8653f3c6811
i.udalov <i.udalov@rambler-co.ru>
2022-12-15 23:18:38 +0300

    Add kv store

--- day1/kv_store/test/kv_store_test.exs
+++ day1/kv_store/test/kv_store_test.exs
@@ -2,7 +2,16 @@ defmodule KVStoreTest do
   use ExUnit.Case
   doctest KVStore
 
-  test "greets the world" do
-    assert KVStore.hello() == :world
+  setup do
+    {:ok, pid} = KVStore.start(%{a: 1})
+    {:ok, pid: pid}
+  end
+
+  test "get", context do
+    assert KVStore.get(context[:pid], :a) == 1
+  end
+
+  test "put", context do
+    assert KVStore.put(context[:pid], {:b, 2}) == :ok
   end
 end
